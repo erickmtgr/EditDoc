f9e0ea92e1a82dc688dd89fb4250a4ace7976950
Erick <erickma.1234@gmail.com>
2018-02-27 11:58:04 +0100

    El EditText recibe el contenido del EditDoc y lo sustituye en su text field

--- app/src/main/java/montenegro/erick/editdoc/EditDoc.java
+++ app/src/main/java/montenegro/erick/editdoc/EditDoc.java
@@ -6,13 +6,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class EditDoc extends AppCompatActivity {
+
+    private TextView title_label;
+    private TextView text_label;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_doc);
+
+        title_label = (TextView) findViewById(R.id.title_label);
+        text_label = (TextView) findViewById(R.id.text_label);
     }
 
     @Override
@@ -25,20 +34,26 @@ public class EditDoc extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.item_edit_title:
-                Intent intent_Change_title = new Intent(this, EditTextActivity.class);
-                intent_Change_title.putExtra("text", R.id.title_label);
-                startActivityForResult(intent_Change_title, 1);
-
+            case R.id.item_edit_title: {
+                Intent intent = new Intent(this, EditTextActivity.class);
+                intent.putExtra("text", title_label.getText().toString());
+                startActivityForResult(intent, 0);
                 break;
-
-            case R.id.item_edit_text:
-                Intent intent_Change_text = new Intent(this, EditTextActivity.class);
-                intent_Change_text.putExtra("text", R.id.text_label);
-                startActivityForResult(intent_Change_text, 1);
-
+            }
+            case R.id.item_edit_text: {
+                Intent intent = new Intent(this, EditTextActivity.class);
+                intent.putExtra("text", text_label.getText().toString());
+                startActivityForResult(intent, 1);
                 break;
+            }
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+
+        }
     }
 }
--- app/src/main/java/montenegro/erick/editdoc/EditTextActivity.java
+++ app/src/main/java/montenegro/erick/editdoc/EditTextActivity.java
@@ -3,16 +3,24 @@ package montenegro.erick.editdoc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EditTextActivity extends AppCompatActivity {
+
+    private TextView edit_text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_title);
 
+        edit_text = (TextView) findViewById(R.id.edit_text);
+
+
         Intent intent = getIntent();
         String text = intent.getStringExtra("text");
+        edit_text.setText(text);
 
 
 
