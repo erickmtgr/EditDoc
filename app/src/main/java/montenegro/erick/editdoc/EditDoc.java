6e9e0432efcefeb09f67d783bc7ff5ce0f3f6352
ericktmtgr <erickma.1234@gmail.com>
2018-02-28 16:01:56 +0100

    Prueba en el ordenador de casa

--- app/src/main/java/montenegro/erick/editdoc/EditDoc.java
+++ app/src/main/java/montenegro/erick/editdoc/EditDoc.java
@@ -3,9 +3,12 @@ package montenegro.erick.editdoc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -14,6 +17,7 @@ public class EditDoc extends AppCompatActivity {
 
     private TextView title_label;
     private TextView text_label;
+    private Button btn_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,12 @@ public class EditDoc extends AppCompatActivity {
 
         title_label = (TextView) findViewById(R.id.title_label);
         text_label = (TextView) findViewById(R.id.text_label);
+        btn_save = (Button) findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your handler code here
+            }
+        });
     }
 
     @Override
@@ -53,6 +63,14 @@ public class EditDoc extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
+            case R.id.item_edit_title:
+
+
+                break;
+
+            case R.id.item_edit_text:
+
+                break;
 
         }
     }
